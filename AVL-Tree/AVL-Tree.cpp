d5f31cfb928e138c071b59ce8d52c98a022d0673
jdeaton <jdeaton@illumina.com>
2017-10-02 17:05:49 -0700

    Implemented AVL tree insertion. General progress.

--- AVL-Tree/AVL-Tree.cpp
+++ AVL-Tree/AVL-Tree.cpp
@@ -4,52 +4,184 @@
  * Presents the template implementation of the self-balancing Binary Search tree.
  */
 
+
 template <class T>
 AVLTree<T>::AVLTree() {}
 
 template <class T>
 void AVLTree<T>::insert(const T& element) {
-  root = insert(root, elemnt);
+    root = insert(*root, elemnt);
 }
 
 template <class T>
-void AVLTree<T>::rightRotate(const BinaryNode<T>& node) {
-
-  BinaryNode<T>& x = node.left();
-  BinaryNode<T>& T2 = x.right();
-
+void AVLTree<T>::remove(const T& element) {
+    root = remove(*root, element);
 }
 
 template <class T>
-std::shared_ptr<BinaryNode<T>> AVLTree<T>::insert(BinaryNode<T>& node, const T& value) {
-
-  if (value < node.value) node.left = insert(node.left, value);
-  if (value > node.value) node.right = insert(node.right, value);
-  else return node;// no duplicates
-
-  updateHeight(node);
-
-
-  int netBalance = getNetBalance(node);
-  if (netBalance > 1) { // right is heavy
-    int rightBalance = getNetBalance(*node.right);
-    if (rightBalance < 0) node.right = rotateRight(node.right);
-    return leftRotate(node);
-  } else if ( netBalance < 1) { // left is heavy
-    int leftBalance = getNetBalance(*node.left);
-    if (leftBalance > 0) node.left = rotateLeft(node.left);
-    return rightRotate(node);
-  }
-
-  return node;
+BSTNode AVLTree<T>::search(const T& element) {
+    return search(root, element);
 }
 
+// Private members
+
+/**
+ * Private Method: rotateLeft
+ * --------------------------
+ * Rotates the tree counter-clockwise, returning the new root
+ * @tparam T: Type of element stored in the tree node
+ * @param node: Pointer to a binary search tree node to rotate
+ * @return: The new root after having been rotated
+ */
+template <class T>
+BSTNode AVLTree<T>::rotateLeft(BSTNode node) {
+    if (node.right == nullptr) return node; // Nothing to rotate onto
+    auto r = node.right;
+    node.right = node.right->left;
+    r.left = node;
+    return r;
+}
+
+/**
+ * Private Method: rotateRight
+ * --------------------------
+ * Rotates the tree clockwise, returning the new root
+ * @tparam T: Type of element stored in the tree node
+ * @param node: Pointer to a binary search tree node to rotate
+ * @return: The new root after having been rotated
+ */
+template <class T>
+BSTNode AVLTree<T>::rotateRight(BSTNode node) {
+    if (node.left == nullptr) return node; // Nothing to rotate onto
+    auto l = node.left;
+    node.left = node.left->right;
+    l.right = node;
+    return l;
+}
+
+/**
+ * Private Method: insert
+ * ----------------------
+ * Inserts an element into a node of a binary search tree, and balances the tree
+ * @tparam T: The type of element contained in the node of the BST
+ * @param node: Root node to insert element into. If nullptr, a new node will be created and returned
+ * @param value: The value to store in the BST
+ * @return: The (possibly new) root of the BST after insertion and balancing
+ */
+template <class T>
+BSTNode AVLTree<T>::insert(BSTNode node, const T& value) {
+    if (node == nullptr) return std::shared_ptr<BinaryNode<T>>(new BinaryNode(value));
+
+    if (value < node.value) node.left = insert(*node.left, value);
+    if (value > node.value) node.right = insert(*node.right, value);
+    else return node; // no duplicates
+
+    updateHeight(node);
+    return balance(node);
+}
+
+/**
+ * Private Method: remove
+ * ----------------------
+ * Removes a specified element from a binary search tree (BST) and re-balances the tree
+ * @tparam T: Type of the element stored in the BST
+ * @param node: The node to remove the value from
+ * @param value: The value to remove the the node
+ * @return: A new node after the removal and rebalancing
+ */
+template <class T>
+BSTNode AVLTree<T>::remove(BSTNode node, const T& value) {
+    if (node == nullptr) return nullptr;
+
+    if (value < node.value) node.left = remove(node.left, value);
+    if (value > node.value) node.right = remove(node.right, value);
+
+    if (node.value == value) { // found it
+        if (node.left == nullptr) {
+            BSTNode temp = node.right;
+            delete node;
+            return temp;
+        } else if (node.right == nullptr) {
+            BSTNode temp = node.left;
+            delete node;
+            return temp;
+        } else {
+
+        }
+
+        BSTNode successor = successorOf()
+    }
+
+    return balance(node);
+}
+
+/**
+ * Private Method: search
+ * ----------------------
+ * If the element is stored in the binary search tree, then the node containing the element
+ * will be returned, otherwise a null pointer will be returned.
+ * @tparam T: Type of the elements stored in the BST
+ * @param node: Root node to search for the element in.
+ * @param element:
+ * @return
+ */
+template <class T>
+BSTNode AVLTree<T>::search(BSTNode node, const T& element) {
+    if (node == nullptr) return nullptr;
+    if (node->value == element) return node;
+    if (element < node->value) return search(node.left, element);
+    if (element > node->value) return search(node.right, element);
+    return nullptr;
+}
+
+/**
+ * Private Method: balance
+ * -----------------------
+ * Balances the AVL tree so that
+ * @tparam T
+ * @param node
+ * @return
+ */
+template <class T>
+BSTNode AVLTree<T>::balance(BSTNode node) {
+    if (node == nullptr) return node;
+
+    int netBalance = getNetBalance(node);
+    if (netBalance > 1) { // right is heavy
+        int rightBalance = getNetBalance(node.right);
+        if (rightBalance < 0) node.right = rotateRight(node.right);
+        return leftRotate(node);
+    } else if (netBalance < 1) { // left is heavy
+        int leftBalance = getNetBalance(node.left);
+        if (leftBalance > 0) node.left = rotateLeft(node.left);
+        return rightRotate(node);
+    } else return node;
+}
+
+/**
+ * Private Method: updateHeight
+ * ----------------------------
+ * Updates the height of a node to be the one plus the
+ * maximum of the heights of it's two children. Nodes with
+ * no children have a height of zero.
+ * @param node: A node to update the height on
+ */
 void AVLTree<T>::updateHeight(BinaryNode<T>& node) {
-  size_t lh = node.left->height;
-  size_t rh = node.right->height;
-  node.height = 1 + (lh > rh ? lh : rh);
+    int lh = node.left == nullptr ? -1 : node.left->height;
+    int rh = node.right == nullptr ? -1 : node.right->height;
+    node.height = 1 + (lh > rh ? lh : rh);
 }
 
-void AVLTree<T>::getNetBalance(const BinaryNode<T>& node) {
-  return node.right->height - node.left->height;
+/**
+ * Private Method: getNetBalance
+ * -----------------------------
+ * Gets the difference in heights of the right and left children of a node
+ * @param node: Node to get the net balance at
+ * @return: Difference between height of right and left children
+ */
+int AVLTree<T>::getNetBalance(const BinaryNode<T>& node) {
+    if (node.height == 0) return 0;
+    if (node.right == nullptr) return - node.left->height;
+    if (node.left == nullptr) return node.right->height;
+    return node.right->height - node.left->height;
 }
--- AVL-Tree/AVL-Tree.h
+++ AVL-Tree/AVL-Tree.h
@@ -1,34 +1,37 @@
-/*
+/**
  * File: AVL-tree.h
  * ----------------
  * Interface of a self-balancing Binary Search tree.
- *
- * This tree is self-balancing in that
- *
  */
 
 #ifndef _avl_tree_h
 #define _avl_tree_h
 
 #include "BinaryNode.h"
+#include <memory>
+
+template <class T>
+typedef std::shared_ptr<BinaryNode<T>> BSTNode;
 
 template <class T>
 class AVLTree {
 
 public:
-
     explicit AVLTree();
 
     void insert(const T& element);
     void remove(const T& element);
-    void contains(const T& query);
+    BSTNode search(const T& element);
 
 private:
-    std::shared_ptr<BinaryNode<T>> root;
+    BSTNode root;
 
-    void leftRotate(const BinaryNode<T>& node);
-    void rightRotate(const BinaryNode<T>& node);
+    BSTNode leftRotate(BSTNode node);
+    BSTNode rightRotate(BSTNode node);
 
+    BSTNode insert(BSTNode node, const T& element);
+    BSTNode remove(BSTNode node, const T& element);
+    BSTNode search(const BSTNode node, const T& element);
 };
 
 #include "AVL-Tree.cpp"
