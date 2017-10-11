776366f4c0ab6fae56f2c05c39bee3ba292274b3
Jon Deaton <jonpdeaton@gmail.com>
2017-10-10 17:07:58 -0700

    Increased test rigour.., found more bugs

--- Data-Structures/RedBlackTree/test-RedBlackTree.cpp
+++ Data-Structures/RedBlackTree/test-RedBlackTree.cpp
@@ -10,17 +10,31 @@
 
 using namespace std;
 
-int main() {
-  RedBlackTree<int> tree;
-
-  // Basic insertion check
-  for (int i = 0; i < 14; i++) {
+void basicInsertion() {
+    RedBlackTree<int> tree;
+    // Basic insertion check
+    for (int i = 0; i < 14; i++) {
     tree.insert(i);
     if (!tree.verify())
       cout << "Error: " << i << endl;
+    }
+    tree.clear();
+}
+
+
+int main() {
+    
+    basicInsertion();
+    cout << "Finished ascending insertion." << endl;
+
+RedBlackTree<int> tree;
+  for (int i = 13; i >= 0; i--) {
+      tree.insert(i);
+      if (!tree.verify())
+          cout << "Error: " << i << endl;
   }
-  tree.clear();
-  cout << "Finished basic insertion" << endl;
+
+  cout << "Finished descending insertion." << endl;
 
   // Mixed insertion test
   for (int i = 0; i < 100; i++) {
@@ -42,7 +56,30 @@ int main() {
     if (!tree.verify())
       cout << "Error: " << i << endl;
   }
+  tree.clear();
+  
+  cout << "Finishes basic removal." << endl;
+  
+  
+  for (int i = 0; i < 100000; i++) {
+      tree.insert((int) random());
+      tree.insert((int) random());
+      tree.remove((int) random());
+      if (!tree.verify())
+          cout << "Error: " << i << endl;
+  }
+  cout << "Finished randomixed insertion and removal" << endl;
+
+
+  for (int i = 1000; i > 0; i--) {
+      tree.remove(i);
+      if (!tree.verify())
+          cout << "Error: " << i << endl;
+  }
+  
+  cout << "Finished descending removal." << endl;
 
   tree.clear();
+
   return 0;
 }
