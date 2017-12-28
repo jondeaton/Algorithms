35a2b27aa2592a6b12daacd24efa50e4102393f3
Jon Deaton <jonpauldeaton@gmail.com>
2017-12-27 23:38:48 -0500

    Style improvement

--- Math/newton.cpp
+++ Math/newton.cpp
@@ -2,26 +2,22 @@
 #include <cassert>
 #include <cmath> 
 
-float my_function(float x)
-{
+float my_function(float x) {
 	float f = x * std::exp(x) - 5;
 	return f;
 }
 
-float diff_func(float (*func)(float), float x, float h=0.0001)
-{
+float diff_func(float (*func)(float), float x, float h=0.0001) {
 	return (func(x + h) - func(x - h)) / (2 * h);
 }
 
-float newton(float (*func)(float), float x0, float tol=0.0001)
-{
+float newton(float (*func)(float), float x0, float tol=0.0001) {
 	float f = func(x0);
 	float df = diff_func(func, x0);
 	assert(df != 0);
 	float x1 = x0 - (f / df);
 	float error = std::fabs(x1 - x0);
-	while (error > tol)
-	{
+	while (error > tol) {
 		x0 = x1;
 		f = func(x0);
 		df = diff_func(func, x0);
@@ -32,8 +28,7 @@ float newton(float (*func)(float), float x0, float tol=0.0001)
 	return x1;
 }
 
-int main(int argc, char* argv[])
-{
+int main(int argc, char* argv[]) {
 	float root = newton(my_function, 1);
 	printf("f(%f) ~= 0\n",root);
 }
