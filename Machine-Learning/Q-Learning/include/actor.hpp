974dc4f771ba9e7340f161ad241686ba87c76d03
Jon Deaton <jonpauldeaton@gmail.com>
2018-01-01 19:11:52 -0500

    Added main and CMake

--- Machine-Learning/Q-Learning/include/actor.hpp
+++ Machine-Learning/Q-Learning/include/actor.hpp
@@ -7,15 +7,17 @@
 
 #include <tuple>
 
-template <class State, class Action, class Environment>
+template <class State, class Action, size_t N, class Environment>
 class actor {
 public:
-  const actions get_actions();
-  virtual const std::tuple<State, Reward> take_action(const Action& action);
+  typedef typename Environment::reward_type reward_type;
+
+  const Action* get_actions();
+  virtual const std::tuple<State, reward_type> take_action(const Action& action);
 
 private:
   State _currentState;
-  Action _actions[5];
+  Action _actions[N];
 };
 
 class maze_actor : actor {
