ad45aec37ca594c5e50c82bc2570d61e62cf484f
Jon Deaton <jonpauldeaton@gmail.com>
2018-07-13 13:47:43 -0700

    Syntax error in hold water. Now its shorter

--- Puzzles/HoldWater.scala
+++ Puzzles/HoldWater.scala
@@ -7,8 +7,7 @@ import math._
 def water(heights: Seq[Int]): Int = {
 	val leftMax = heights.scanLeft(0)(max)
 	val rightMax = heights.scanRight(0)(max)
-	(heights, leftMax, rightMax).zipped.map(t => {
-		val (h, l, r) = t
-    	max(0, min(l, r) - h)
-	}).sum
+	(heights, leftMax, rightMax).zipped.map((h, l, r) =>
+		max(0, min(l, r) - h)
+	).sum
 }
