5617d15ee3f9e566f83d9e10cef57932bd4133a5
Jon Deaton <jonpauldeaton@gmail.com>
2018-07-18 00:31:56 -0700

    Refactoring

--- Data-Structures/Clib/include/cmap.h
+++ Data-Structures/Clib/include/cmap.h
@@ -14,15 +14,39 @@ typedef unsigned int (*CMapHashFn)(const void *key, size_t keysize);
 typedef int (*CMapCmpFn)(const void *keyA, const void *keyB, size_t keysize);
 typedef struct CMapImplementation CMap;
 
+int string_cmp(const void *a, const void *b, size_t keysize);
 
+/**
+ * Create a HashTable in a dynamically allocated region of memory.
+ * @param key_size size of all keys stored in HashTable
+ * @param value_size size of all values stored in the HashTable
+ * @param hash Hash function used to hash keys, may be NULL
+ * @param cmp Comparison function between keys, may be NULL
+ * @param cleanupKey Cleanup function for keys, may be NULL
+ * @param cleanupValue Cleanup function for values for, may be NULL
+ * @param capacity number of values that maybe stored in table
+ * @return Pointer to a hash table in dynamically allocated memory
+ */
 CMap *cmap_create(size_t key_size, size_t value_size,
                   CMapHashFn hash, CMapCmpFn cmp,
                   CleanupFn cleanupKey, CleanupFn cleanupValue,
                   unsigned int capacity_hint);
 
+/**
+ * Dispose of a Hash Table created from cmap_create
+ * @param cm Pointer to hash table
+ */
 void cmap_dispose(CMap *cm);
+
+/**
+ * The number of key value pairs currently stored in the table
+ * @param cm Pointer to hash table
+ * @return Number of elements stored in the hash table
+ */
 unsigned int cmap_count(const CMap *cm);
+
 void *cmap_insert(CMap *cm, const void *key, const void *value);
+
 void *cmap_lookup(const CMap *cm, const void *key);
 void cmap_remove(CMap *cm, const void *key);
 void cmap_clear(CMap *cm);
--- Data-Structures/Clib/src/cmap.c
+++ Data-Structures/Clib/src/cmap.c
@@ -4,6 +4,8 @@
  */
 
 #include "cmap.h"
+#include "hash.h"
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <signal.h>
@@ -13,6 +15,7 @@
 
 // a suggested value to use when given capacity_hint is 0
 #define DEFAULT_CAPACITY 1024
+#define unused __attribute__ ((unused))
 
 /**
  * @struct CMapImplementation
@@ -27,10 +30,10 @@ struct CMapImplementation {
   size_t key_size;              // The size of each key
   size_t value_size;            // The size of each value
 
-  CleanupFn cleanupKey;      // Callback for key disposal
-  CleanupFn cleanupValue;  // Callback for value disposal
+  CleanupFn cleanupKey;         // Callback for key disposal
+  CleanupFn cleanupValue;       // Callback for value disposal
   CMapHashFn hash;              // hash function callback
-  CMapCmpFn cmp;                // key comparison funciton
+  CMapCmpFn cmp;                // key comparison function
 };
 
 /**
@@ -38,12 +41,12 @@ struct CMapImplementation {
  * Stores metadata about a single key-value pair
  */
 struct entry {
-  unsigned int origin;      // origin bucket
   unsigned int hash;        // hash of key
   uint8_t status;           // status bits
   char kv[];                // Key/value pair
 };
 
+// Macros/functions for setting entry status bits
 #define FREE_MASK ((uint8_t) 1)
 
 static inline bool is_free(const struct entry* e) {
@@ -58,34 +61,19 @@ static inline void set_free(struct entry *e, bool free) {
 // static function declarations
 static inline size_t entry_size(const CMap *cm);
 static inline struct entry *get_entry(const CMap *cm, unsigned int index);
+static struct entry *lookup_key(const CMap *cm, const void *key);
+static inline void *value_of(const CMap *cm, const struct entry *entry);
+static inline void *key_of(const struct entry *entry);
+static inline void move(CMap *cm, struct entry *entry1, struct entry *entry2);
+static void erase(CMap *cm, struct entry *e);
+static void delete(CMap *cm, unsigned int start, unsigned int stop);
+static int lookup_index(const CMap *cm, const void *key);
 
-/**
- * @breif Default hash function
- * @detail This function adapted from Eric Roberts' _The Art and Science of C_
- * The hash code is computed using a method called "linear congruence."
- * A similar function using this method is described on page 144 of Kernighan
- * and Ritchie. The choice of the value for the multiplier can have a significant
- * effort on the performance of the algorithm, but not on its correctness.
- * The computed hash value is stable, e.g. passing the same string and
- * nbuckets to function again will always return the same code.
- */
-static unsigned int default_hash(const void *key, size_t keysize) {
-  const unsigned long MULTIPLIER = 2630849305L; // magic number
-  unsigned long hashcode = 0;
-  for (unsigned int i = 0; i < keysize; i++)
-    hashcode = hashcode * MULTIPLIER + ((const char*) key)[i];
-  return hashcode;
+
+int string_cmp(const void *a, const void *b, size_t keysize unused) {
+  return strcmp(*(const char **) a, *(const char **) b);
 }
 
-
-/**
- * @breif Instantiate a CMap struct in a dynamically allocated region of memory.
- * This function will also fill in the relevant elements of the CMap
- * @param value_size
- * @param capacity
- * @param fn
- * @return
- */
 CMap *cmap_create(size_t key_size, size_t value_size,
                   CMapHashFn hash, CMapCmpFn cmp,
                   CleanupFn cleanupKey, CleanupFn cleanupValue,
@@ -97,7 +85,13 @@ CMap *cmap_create(size_t key_size, size_t value_size,
 
   cm->key_size = key_size;
   cm->value_size = value_size;
+  cm->size = 0;
+  cm->cleanupKey = cleanupKey;
+  cm->cleanupValue = cleanupValue;
+  cm->hash = hash == NULL ? roberts_hash : hash;
+  cm->cmp = cmp == NULL ? memcmp : cmp;
 
+  // Allocate array for key-value entries
   cm->capacity = capacity > 0 ? capacity : DEFAULT_CAPACITY;
   cm->entries = malloc(cm->capacity * entry_size(cm));
   if (cm->entries == NULL) {
@@ -105,82 +99,24 @@ CMap *cmap_create(size_t key_size, size_t value_size,
     return NULL;
   }
 
-  cm->size = 0;
-
+  // Set all the entries to free
   for (unsigned int i = 0; i < cm->capacity; ++i) {
     struct entry *e = get_entry(cm, i);
     assert(e != NULL);
     set_free(e, true);
   }
 
-  cm->cleanupKey = cleanupKey;
-  cm->cleanupValue = cleanupValue;
-  cm->hash = hash == NULL ? default_hash : hash;
-  cm->cmp = cmp == NULL ? memcmp : cmp;
-
   return cm;
 }
 
-/**
- * @brief Returns the number of key value pairs currently
- * stored in the table
- */
+void cmap_dispose(CMap* cm) {
+  cmap_clear(cm);
+  free(cm->entries);
+  free(cm);
+}
+
 unsigned int cmap_count(const CMap* cm) {
   return cm->size;
-}
-
-/**
- * @breif Returns the maximum capacity of the hash table
- */
-unsigned int cmap_capacity(const CMap* cm) {
-  return cm->capacity;
-}
-
-/**
- * @breif Gets the size of each entry in the Hash Table
- * @param cm
- * @return
- */
-static inline size_t entry_size(const CMap *cm) {
-  return sizeof(struct entry) + cm->key_size + cm->value_size;
-}
-
-static inline struct entry *get_entry(const CMap *cm, unsigned int index) {
-  assert(cm != NULL);
-  assert(index >= 0 && index < cm->capacity);
-  void *entry = (char *) cm->entries + index * entry_size(cm);
-  return (struct entry *) entry;
-}
-
-/**
- * @breif Finds the entry for this key
- * @param cm The CMap to lookup the key in
- * @param key the key to lookup in the CMap
- * @return Pointer
- */
-static struct entry *lookup_key(const CMap *cm, const void *key) {
-  unsigned int hash = cm->hash(key, cm->key_size);
-
-  for (unsigned int i = 0; i < cm->capacity; ++i) {
-    struct entry *e = get_entry(cm, (hash + i) % cm->capacity);
-    if (e == NULL || is_free(e)) continue;
-
-    // Use cached hash value to do an easy/cache-friendly comparison
-    if (e->hash != hash) continue;
-
-    // Only dereference to compare full keys if you have to
-    if (cm->cmp(&e->kv, key, cm->key_size) == 0)
-      return e;
-  }
-  return NULL; // Went all the way around
-}
-
-static inline void *value_of(const CMap *cm, const struct entry *entry) {
-  return (char *) &entry->kv + cm->key_size;
-}
-
-static inline void *key_of(const struct entry *entry) {
-  return (void *) (&entry->kv);
 }
 
 /**
@@ -214,7 +150,6 @@ void *cmap_insert(CMap *cm, const void *key, const void *value) {
 
   set_free(entry, false);
   entry->hash = hash;
-  entry->origin = hash % cm->capacity;
 
   cm->size++;
   return entry;
@@ -232,6 +167,117 @@ void *cmap_lookup(const CMap *cm, const void *key) {
   struct entry *entry = lookup_key(cm, key);
   if (entry == NULL) return NULL;
   return value_of(cm, entry);
+}
+
+/**
+ * @breif Removes a key-value pair from the hash table
+ * @param cm Hash table to remove the key value pair from
+ * @param key The key to remove
+ */
+void cmap_remove(CMap *cm, const void *key) {
+  if (cm == NULL || key == NULL) return;
+
+  int start = lookup_index(cm, key);
+  if (start < 0) return;
+  struct entry *e = get_entry(cm, start);
+  assert(e != NULL);
+
+  erase(cm, e);
+  delete(cm, start, e->hash % cm->capacity);
+  cm->size--;
+}
+
+/**
+ * @breif Removes all of the elements from the hash tabls
+ * @param cm The CMap to remove all the elements from
+ */
+void cmap_clear(CMap *cm) {
+  if (cm == NULL) return;
+
+  unsigned int num_cleared = 0;
+  for (unsigned int i = 0; i < cm->capacity; ++i) {
+    struct entry *e = get_entry(cm, i);
+    assert(e != NULL);
+    if (is_free(e)) continue;
+
+    erase(cm, e);
+    num_cleared++;
+
+    // If we've deleted them all, stop and save a bit of searching
+    if (num_cleared == cm->size) break;
+  }
+  cm->size = 0;
+}
+
+const void *cmap_first(const CMap *cm) {
+  if (cm == NULL) return NULL;
+  if (cm->size == 0) return NULL;
+
+  for (unsigned int i = 0; i < cm->capacity; ++i) {
+    struct entry *e = get_entry(cm, i);
+    if (!is_free(e)) return key_of(e);
+  }
+  return NULL;
+}
+
+/**
+ * @breif Gets a pointer to the next key
+ * @param cm The hash table
+ * @param prevkey The previous key
+ * @return pointer to the next key, if there is one.
+ */
+const void *cmap_next(const CMap *cm, const void *prevkey) {
+  if (cm == NULL || prevkey == NULL) return NULL;
+
+  const struct entry *e = (struct entry *) ((char *) prevkey - offsetof(struct entry, kv));
+  while (e != cm->end) {
+    e = (struct entry *) ((char *) e + entry_size(cm));
+    if (!is_free(e)) return key_of(e);
+  }
+  return NULL;
+}
+
+static inline size_t entry_size(const CMap *cm) {
+  return sizeof(struct entry) + cm->key_size + cm->value_size;
+}
+
+static inline struct entry *get_entry(const CMap *cm, unsigned int index) {
+  assert(cm != NULL);
+  assert(index >= 0 && index < cm->capacity);
+  void *entry = (char *) cm->entries + index * entry_size(cm);
+  return (struct entry *) entry;
+}
+
+/**
+ * @breif Finds the entry for this key
+ * @param cm The CMap to lookup the key in
+ * @param key the key to lookup in the CMap
+ * @return Pointer
+ */
+static struct entry *lookup_key(const CMap *cm, const void *key) {
+  if (cm->size == 0) return NULL;
+
+  unsigned int hash = cm->hash(key, cm->key_size);
+  for (unsigned int i = 0; i < cm->capacity; ++i) {
+    struct entry *e = get_entry(cm, (hash + i) % cm->capacity);
+    if (e == NULL || is_free(e)) continue;
+
+    // Use cached hash value to do an easy/cache-friendly comparison
+    if (e->hash != hash) continue;
+
+    // Only dereference to compare full keys if you have to
+    if (cm->cmp(&e->kv, key, cm->key_size) == 0)
+      return e;
+  }
+  return NULL; // Went all the way around
+}
+
+static inline void *value_of(const CMap *cm, const struct entry *entry) {
+  return (char *) &entry->kv + cm->key_size;
+}
+
+static inline void *key_of(const struct entry *entry) {
+  return (void *) (&entry->kv);
 }
 
 static inline void move(CMap *cm, struct entry *entry1, struct entry *entry2) {
@@ -264,7 +310,7 @@ static void delete(CMap *cm, unsigned int start, unsigned int stop) {
     if (is_free(next)) return; // reached the end
 
     // Found one that can replace
-    if (next->origin >= start) {
+    if ((next->hash) % cm->capacity >= start) {
       move(cm, entry, next);
       set_free(next, true);
       break;
@@ -288,79 +334,4 @@ static int lookup_index(const CMap *cm, const void *key) {
       return (hash + i) % cm->capacity;
   }
   return -1;
-}
-
-/**
- * @breif Removes a key-value pair from the hash table
- * @param cm Hash table to remove the key value pair from
- * @param key The key to remove
- */
-void cmap_remove(CMap *cm, const void *key) {
-  if (cm == NULL || key == NULL) return;
-
-  int start = lookup_index(cm, key);
-  if (start < 0) return;
-  struct entry *e = get_entry(cm, start);
-  assert(e != NULL);
-
-  erase(cm, e);
-  delete(cm, start, e->origin);
-  cm->size--;
-}
-
-void cmap_dispose(CMap* cm) {
-  cmap_clear(cm);
-  free(cm->entries);
-  free(cm);
-}
-
-/**
- * @breif Removes all of the elements from the hash tabls
- * @param cm The CMap to remove all the elements from
- */
-void cmap_clear(CMap *cm) {
-  if (cm == NULL) return;
-
-  unsigned int num_cleared = 0;
-  for (unsigned int i = 0; i < cm->capacity; ++i) {
-    struct entry *e = get_entry(cm, i);
-    assert(e != NULL);
-    if (is_free(e)) continue;
-
-    erase(cm, e);
-    num_cleared++;
-
-    // If we've deleted them all, stop and save a bit of searching
-    if (num_cleared == cm->size) break;
-  }
-  cm->size = 0;
-}
-
-
-const void *cmap_first(const CMap *cm) {
-  if (cm == NULL) return NULL;
-  if (cm->size == 0) return NULL;
-
-  for (unsigned int i = 0; i < cm->capacity; ++i) {
-    struct entry *e = get_entry(cm, i);
-    if (!is_free(e)) return key_of(e);
-  }
-  return NULL;
-}
-
-/**
- * @breif Gets a pointer to the next key
- * @param cm The hash table
- * @param prevkey The previous key
- * @return pointer to the next key, if there is one.
- */
-const void *cmap_next(const CMap *cm, const void *prevkey) {
-  if (cm == NULL || prevkey == NULL) return NULL;
-
-  const struct entry *e = (struct entry *) ((char *) prevkey - offsetof(struct entry, kv));
-  while (e != cm->end) {
-    e = (struct entry *) ((char *) e + entry_size(cm));
-    if (!is_free(e)) return key_of(e);
-  }
-  return NULL;
 }
