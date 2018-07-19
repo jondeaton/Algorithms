6012356e3484adbd75ceb7d03b29f6b2cf805d18
Jon Deaton <jonpauldeaton@gmail.com>
2018-07-18 19:02:25 -0700

    Added BloomFilter placeholder

---
+++ Data-Structures/BloomFilter/include/bloom-filter.hpp
@@ -0,0 +1,17 @@
+/**
+ * @file bloom-filter.hpp
+ * Presents the implementation of a Bloom Filter
+ */
+
+
+template <typename T, int K>
+class bloom_filter {
+
+public:
+  typedef uint64_t HashKey
+
+private:
+  HashKey hash_keys[K]; // represents the K different hash functions
+
+}
+
---
+++ Data-Structures/BloomFilter/test/test-bloom-filter.cpp
@@ -0,0 +1,8 @@
+#include "bloom_filter.hpp"
+
+int main(int argc, char *argv[]) {
+
+
+    return 0;
+}
+
