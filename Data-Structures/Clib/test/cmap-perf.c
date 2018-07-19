e0a4f43577d56e21a2850d32a0cf88211d1138d8
Jon Deaton <jonpauldeaton@gmail.com>
2018-07-18 19:02:01 -0700

    Expanded tests a little bit

--- Data-Structures/Clib/test/cmap-perf.c
+++ Data-Structures/Clib/test/cmap-perf.c
@@ -1,4 +1,5 @@
 
+#include "cmap.h"
 
 int main() {
 
--- Data-Structures/Clib/test/cmap_test.c
+++ Data-Structures/Clib/test/cmap_test.c
@@ -8,7 +8,52 @@
 
 #define unused __attribute__ ((unused))
 
-void functionality_test() {
+// Testing to make sure that you can use all kinds
+// of key and value sizes
+static bool test_creation() {
+  for (size_t keysize = 1; keysize <= 32; keysize++) {
+    for (size_t valuesize = 1; valuesize; valuesize++) {
+
+      CMap *map = cmap_create(keysize, valuesize,
+                              NULL, NULL, NULL, NULL, 0);
+
+      if (map == NULL)
+        return false;
+
+      if (cmap_count(map) != 0)
+        return false;
+
+      cmap_dispose(map);
+    }
+  }
+ return true;
+}
+
+static bool test_insertion() {
+  CMap *map = cmap_create(sizeof(char *), sizeof(int),
+                          string_hash, string_cmp,
+                          NULL, NULL, 0);
+
+  assert(map != NULL);
+  assert(cmap_count(map) == 0);
+
+  const char *a = "a";
+  const char *b = "b";
+
+  cmap_insert(map, &a, &(int){1});
+  cmap_insert(map, &b, &(int){2});
+
+  assert(cmap_count(map) == 2);
+
+  // Check to make sure we can get values
+  const int *value = cmap_lookup(map, &a);
+  assert(*value == 1);
+
+  return true;
+}
+
+
+static bool test_deletion() {
   CMap *map = cmap_create(sizeof(char *), sizeof(int),
                           string_hash, string_cmp,
                           NULL, NULL, 0);
@@ -43,6 +88,13 @@ void functionality_test() {
   assert(cmap_lookup(map, &a) == NULL);
   cmap_dispose(map);
 
+  return true;
+}
+
+void functionality_test() {
+  test_creation();
+  test_insertion();
+  test_deletion();
 }
 
 int main (int argc unused, char* argv[] unused) {
